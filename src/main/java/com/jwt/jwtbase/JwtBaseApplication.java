3f185a966d19d509333195fb46ac7d5277d2f89c
Luis Lopes <luismvlopes@gmail.com>
2023-11-24 12:02:02 +0000

    initial commit

---
+++ src/main/java/com/jwt/jwtbase/JwtBaseApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/jwtbase/JwtBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
